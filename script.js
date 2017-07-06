5962332ddc6d1adaada801fe3d731a0a0a0116a2
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-06 14:09:45 +0300

    Added getUserMedia

---
+++ script.js
@@ -0,0 +1,79 @@
+/*
+ * Websensor video stabilization project
+ * https://github.com/jessenie-intel/websensor-video
+ *
+ * Copyright (c) 2017 Jesse Nieminen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+
+
+'use strict';
+
+var constraints = {audio: true,video: {  width: { min: 640, ideal: 640, max: 640 },  height: { min: 480, ideal: 480, max: 480 }}};
+var mediaRecorder = null;
+var chunks = [];
+
+var videoElement = document.querySelector('video');
+
+function errorCallback(error){
+	console.log("error: ", error);	
+}
+
+function startDemo () {
+		navigator.getUserMedia(constraints, startRecording, errorCallback);
+	}
+
+function startRecording(stream) {
+	        var options = {mimeType: 'video/webm;codecs=vp9'};
+		mediaRecorder = new MediaRecorder(stream, options);
+		//mediaRecorder = new MediaRecorder(stream);
+
+	        mediaRecorder.start(10);
+
+	        var url = window.URL;
+	        videoElement.src = url ? url.createObjectURL(stream) : stream;
+	        videoElement.play();
+
+	        mediaRecorder.ondataavailable = function(e) {
+		        chunks.push(e.data);
+	        };
+
+	        mediaRecorder.onerror = function(e){
+		        console.log('Error: ', e);
+	        };
+
+
+	        mediaRecorder.onstart = function(){
+	        };
+
+	        mediaRecorder.onstop = function(){
+		        var blob = new Blob(chunks, {type: "video/webm"});
+		        chunks = [];
+
+		        var videoURL = window.URL.createObjectURL(blob);
+
+		        videoElement.src = videoURL;
+	        };
+
+	        mediaRecorder.onpause = function(){
+	        }
+
+	        mediaRecorder.onresume = function(){
+	        }
+
+	        mediaRecorder.onwarning = function(e){
+	        };
+}
