fc884c93f30a060ff2cccc33f06062864c0e9a00
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-10 16:21:20 +0300

    Working on reacting to sensor input

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ function startRecording(stream) {
 
 	        var url = window.URL;
 	        videoElement.src = url ? url.createObjectURL(stream) : stream;	        
-                //videoElement.play();
+                videoElement.play();
 
 	        mediaRecorder.ondataavailable = function(e) {
                         //console.log("Data available", e);
