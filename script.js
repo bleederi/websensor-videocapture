b48ce8c707a9d68fe49077228fb3007971f5f1f2
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-13 12:34:05 +0300

    Blob URL stuff

--- script.js
+++ script.js
@@ -309,7 +309,7 @@ function startRecording(stream) {
 		        var blob = new Blob(chunks, {type: "video/webm"});
 		        chunks = [];
 
-		        videoURL = window.URL.createObjectURL(blob) + "#t=0,2&xywh=160,120,320,240";
+		        videoURL = window.URL.createObjectURL(blob) + "#xywh=pixel:0,0,320,240&t=0,2";
 
 		        videoElement.src = videoURL;
                         videoElement.load();
