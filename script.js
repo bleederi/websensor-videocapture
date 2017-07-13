cf2b43cc6c77f72a2b62416d97af40c6df9e3d5d
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-13 16:15:52 +0300

    Video currentTime test

--- script.js
+++ script.js
@@ -440,7 +440,7 @@ function readFrameData(blob, oriArray) {     //Read video data from blob to obje
                                 //y = 100*oriDiff.roll;
                                 x = videoElement.videoWidth*(oriDiff.yaw/2*(Math.PI));
                                 y = -videoElement.videoHeight*(oriDiff.roll/2*(Math.PI));     //each 2pi means 1 video height
-console.log(((nFrame/dataArray.length)*videoElement.duration).toFixed(3));
+console.log(nFrame/dataArray.length));
                         videoElement.currentTime = parseFloat(((nFrame/dataArray.length)*videoElement.duration).toFixed(3));
                         console.log(videoElement.currentTime);                        
                         //}          
