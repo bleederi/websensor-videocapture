2c4d1e92ff75405582e8691a153bc8e4b526af4b
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-13 15:30:26 +0300

    Video currentTime test

--- script.js
+++ script.js
@@ -481,8 +481,8 @@ function readFrameData(blob, oriArray) {     //Read video data from blob to obje
         videoElement.currentTime = timeFromStart/1000;        //TODO: fix currentTime
         //videoElement.currentTime = (nFrame/dataArray.length)*videoElement.duration;
         //videoElement.src = videoURL;
-        //videoElement.load();
-        //videoElement.play();
+        videoElement.load();
+        videoElement.play();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(videoElement,0,0, videoElement.videoWidth, videoElement.videoHeight);
         ctx.beginPath();
