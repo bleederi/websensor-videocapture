fdbb3b820940ed9a6e7cfb28a1806b5452d61e13
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-13 14:41:12 +0300

    Try to only show part of video

--- script.js
+++ script.js
@@ -379,6 +379,8 @@ function readFrameData(blob, oriArray) {     //Read video data from blob to obje
         //console.log(prevFrame, nFrame);
         //let x = 0;
         //let y = 0;
+  canvas.width = videoElement.videoWidth/2;
+  canvas.height = videoElement.videoHeight/2;
         let dx = 0;
         let dy = 0;
         if(nFrame === 0)
