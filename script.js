d088aed4062236d73041afbe1121e95cfd9769e8
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-14 14:59:48 +0300

    Canvas rotate

--- script.js
+++ script.js
@@ -582,7 +582,7 @@ function readFrameData(blob, oriArray) {     //Read video data from blob to obje
         ctx.drawImage(videoElement,0,0, videoElement.videoWidth, videoElement.videoHeight);
 //ctx.drawImage(videoElement,x+0.1*canvas.width,y+0.1*canvas.height, widthR, heightR, 0, 0, 1.1*canvas.width, 1.1*canvas.height);
 //ctx.drawImage(videoElement,0,0, widthR, heightR, x+0.1*canvas.width, 1.1*canvas.width, y+0.1*canvas.height, 1.1*canvas.height);
-        ctx.translate(trans.x, trans.y);
+        //ctx.translate(trans.x, trans.y);
         ctx.rotate(-angle);
         console.log(angle);
         ctx.beginPath();
@@ -591,7 +591,7 @@ function readFrameData(blob, oriArray) {     //Read video data from blob to obje
         //ctx.rect((x+0.1*canvas.width)+(x*Math.cos(angle) - y*Math.sin(angle)),y+0.1*canvas.height,widthR,heightR);        //rotated and translated rect
         ctx.stroke();
         ctx.rotate(angle);
-        ctx.translate(-trans.x, -trans.y);
+        //ctx.translate(-trans.x, -trans.y);
         }
         if(videoElement.ended)
         {
