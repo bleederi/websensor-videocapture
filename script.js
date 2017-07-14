ad7f111fcb84e8058303c3bf839424ddd6653581
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-14 16:40:15 +0300

    Canvas clipping

--- script.js
+++ script.js
@@ -586,7 +586,7 @@ function readFrameData(blob, oriArray) {     //Read video data from blob to obje
         //videoElement.load();
         //videoElement.play();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(videoElement,0,0, videoElement.videoWidth, videoElement.videoHeight);
+        //ctx.drawImage(videoElement,0,0, videoElement.videoWidth, videoElement.videoHeight);
 //ctx.drawImage(videoElement,x+0.1*canvas.width,y+0.1*canvas.height, widthR, heightR, 0, 0, 1.1*canvas.width, 1.1*canvas.height);
 //ctx.drawImage(videoElement,0,0, widthR, heightR, x+0.1*canvas.width, 1.1*canvas.width, y+0.1*canvas.height, 1.1*canvas.height);
         ctx.translate(trans.x, trans.y);
@@ -595,6 +595,7 @@ function readFrameData(blob, oriArray) {     //Read video data from blob to obje
         //ctx.beginPath();
         ctx.rect(-widthR/2, -heightR/2, widthR, heightR);
         ctx.clip();
+        ctx.drawImage(videoElement,0,0, videoElement.videoWidth, videoElement.videoHeight);
         //ctx.rect(x+0.1*canvas.width,y+0.1*canvas.height,widthR,heightR);
         //ctx.rect((x+0.1*canvas.width)+(x*Math.cos(angle) - y*Math.sin(angle)),y+0.1*canvas.height,widthR,heightR);        //rotated and translated rect
         //ctx.stroke();
