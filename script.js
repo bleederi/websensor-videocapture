d26d5ea83e18be74c52c9f6227ec537f516346c6
Jezze <asd@masd.com>
2017-07-16 21:34:39 +0300

    Canvas clipping

--- script.js
+++ script.js
@@ -595,7 +595,7 @@ function readFrameData(blob, oriArray) {     //Read video data from blob to obje
         ctx.beginPath();
         ctx.rect(trans.x-widthR/2, trans.y-heightR/2, widthR, heightR);
         console.log(trans.x-widthR/2, trans.y-heightR/2, widthR, heightR);
-        var imgData=ctx.getImageData(-widthR/2, -heightR/2, widthR, heightR);
+        //var imgData=ctx.getImageData(-widthR/2, -heightR/2, widthR, heightR);
         //ctx.clip();
         //ctx.rect(x+0.1*canvas.width,y+0.1*canvas.height,widthR,heightR);
         //ctx.rect((x+0.1*canvas.width)+(x*Math.cos(angle) - y*Math.sin(angle)),y+0.1*canvas.height,widthR,heightR);        //rotated and translated rect
