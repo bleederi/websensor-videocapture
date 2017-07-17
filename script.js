f419546bd77756da5a4aac8ba31d2d756333bd17
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-17 12:10:22 +0300

    Canvas clipping

--- script.js
+++ script.js
@@ -596,19 +596,21 @@ function readFrameData(blob, oriArray) {     //Read video data from blob to obje
         ctx.rotate(-angle);
         //console.log(angle);
         ctx.drawImage(videoElement,-videoElement.videoWidth/2,-videoElement.videoHeight/2, videoElement.videoWidth, videoElement.videoHeight);
-        //ctx.clip();
-        //ctx.rect(x+0.1*canvas.width,y+0.1*canvas.height,widthR,heightR);
-        //ctx.rect((x+0.1*canvas.width)+(x*Math.cos(angle) - y*Math.sin(angle)),y+0.1*canvas.height,widthR,heightR);        //rotated and translated rect      
-        ctx.rotate(angle);
-        ctx.translate(-videoElement.videoWidth/2, -videoElement.videoHeight/2);
         ctx.beginPath();
         ctx.rect(-widthR/2, -heightR/2, widthR, heightR);
         console.log(-widthR/2, -heightR/2, widthR, heightR);
         var imgData=ctx.getImageData(-widthR/2, -heightR/2, widthR, heightR);
+        //ctx.clip();
+        //ctx.rect(x+0.1*canvas.width,y+0.1*canvas.height,widthR,heightR);
+        //ctx.rect((x+0.1*canvas.width)+(x*Math.cos(angle) - y*Math.sin(angle)),y+0.1*canvas.height,widthR,heightR);        //rotated and translated rect
+        ctx2.rotate(angle);
+        ctx2.translate(-videoElement.videoWidth/2, -videoElement.videoHeight/2);
         ctx.stroke();
         ctx2.putImageData(imgData,0,0);
         ctx2.rect(0, 0, 100, 100);
-        ctx2.stroke();  
+        ctx2.stroke();        
+        ctx.rotate(angle);
+        ctx.translate(-videoElement.videoWidth/2, -videoElement.videoHeight/2);
         //ctx.clearRect(0, 0, canvas.width, canvas.height);
         }
         if(videoElement.ended)
