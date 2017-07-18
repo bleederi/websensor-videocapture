91e93d168b53e69aad23e60a8ac3aea63526c49b
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-18 17:40:31 +0300

    Build camera path

--- script.js
+++ script.js
@@ -649,9 +649,12 @@ function readFrameData(blob, oriArray, cameraPath) {     //Read video data from 
         //ctx.translate(-(videoElement.videoWidth/2 + videoElement.videoWidth * (2*angle.gamma/(Math.PI)), -videoElement.videoHeight/2);
         ctx.translate(-(videoElement.videoWidth/2 + (1/4) * videoElement.videoWidth * Math.sin(angle.gamma)), -videoElement.videoHeight/2);
         ctx.beginPath();
-        for(let i=0; i<cameraPath.length; i++)
+        if(cameraPath !== undefined)
         {
-                ctx.fillRect(cameraPath[i].x,cameraPath[i].y,3,3);
+                for(let i=0; i<cameraPath.length; i++)
+                {
+                        ctx.fillRect(cameraPath[i].x,cameraPath[i].y,3,3);
+                }
         }
         ctx.rect((canvas.width-widthR)/2,(canvas.height-heightR)/2,widthR,heightR);
         var imgData=ctx.getImageData((canvas.width-widthR)/2,(canvas.height-heightR)/2,widthR,heightR);
