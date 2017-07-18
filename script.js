309bcf6ea0ae629569f2f991aa53ba6a58b4fa6f
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-18 18:06:18 +0300

    Build camera path

--- script.js
+++ script.js
@@ -635,8 +635,6 @@ function readFrameData(blob, oriArray, cameraPath) {     //Read video data from 
                         console.log(dataArray2);
                         stabilize(dataArray2);
                 } */ 
-                        cameraPos = {"x": cameraPath[nFrame].x, "y": cameraPath[nFrame].y}
-                        console.log(cameraPos.x, cameraPos.y);
         //render video and rect
         let widthR = 0.6*videoElement.videoWidth;
         let heightR = 0.6*videoElement.videoHeight;
@@ -649,15 +647,7 @@ function readFrameData(blob, oriArray, cameraPath) {     //Read video data from 
         ctx.rotate(-angle.alpha);
         //ctx.translate(-(videoElement.videoWidth/2 + videoElement.videoWidth * (2*angle.gamma/(Math.PI)), -videoElement.videoHeight/2);
         ctx.translate(-(videoElement.videoWidth/2 + (1/4) * videoElement.videoWidth * Math.sin(angle.gamma)), -videoElement.videoHeight/2);
-        ctx.beginPath();/*
-        if(cameraPath !== undefined) {
-        //console.log(cameraPath);
-        for(let i=0; i<cameraPath.length; i++)
-        {
-                ctx.fillRect(cameraPath[i].x,cameraPath[i].y,3,3);
-                        //console.log(cameraPath[i].x, cameraPath[i].y);
-        }
-        }*/
+        ctx.beginPath();
         ctx.rect((canvas.width-widthR)/2,(canvas.height-heightR)/2,widthR,heightR);
         var imgData=ctx.getImageData((canvas.width-widthR)/2,(canvas.height-heightR)/2,widthR,heightR);
         ctx.stroke();
