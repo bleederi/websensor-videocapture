5b33cb63718afeb57e911731ff8010016ef5ea0c
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-18 16:13:01 +0300

    Fix delay

--- script.js
+++ script.js
@@ -298,7 +298,7 @@ function startSensors() {
                         const zeroBias = 0.02;
                         
                         console.log(accel_sensor.timestamp, gyroscope.timestamp, accel_sensor.timestamp - gyroscope.timestamp);
-                        if(accel_sensor.timestamp - gyroscope.timestamp === 0)
+                        if(accel_sensor.timestamp - gyroscope.timestamp !== 0)
                         {
                         //alpha = (1 - zeroBias) * (alpha + gyroscope.z * dt);                        
                         alpha = alpha + gyro_filtered.z * dt;
