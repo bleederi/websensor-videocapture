9406ec9f4a3b1ad4effda97f93eb43fd6ac9e9c2
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-26 16:17:34 +0300

    Camera path

--- script.js
+++ script.js
@@ -502,8 +502,8 @@ videoElement.addEventListener('play', function() {
         //delay = Math.floor(sensorframeTimeDiff/durationPerFrame);
         //console.log("Delay", delay);
         cameraPath = buildCameraPath(dataArray);     //build camera path
-        tempCameraPath.x = smooth(cameraPath.map(function(a) {return a.x;}), 0.85);       //smoothen the path
-        tempCameraPath.y = smooth(cameraPath.map(function(a) {return a.y;}), 0.85);       //smoothen the path
+        tempCameraPath.x = smooth(cameraPath.map(a => a.x), 0.85);       //smoothen the path
+        tempCameraPath.y = smooth(cameraPath.map(a => a.y), 0.85);       //smoothen the path
         for(let i=0; i<tempCameraPath.length; i++)
         {
                 let xy = {"x": tempCameraPath[i].x, "y": tempCameraPath[i].y};
