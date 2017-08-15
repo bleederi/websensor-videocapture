eb61f04456b613d0166b157ab2b3bd7404ca8579
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-15 13:51:04 +0300

    Fix

--- script.js
+++ script.js
@@ -181,50 +181,6 @@ class LowPassFilterOrientation {
         }
 };
 
-class KalmanFilter {
-  constructor() {
-    this.Q_angle = 0.01;
-    this.Q_gyro = 0.0003;
-    this.R_angle = 0.01;
-
-    this.reset();
-  }
-
-  reset() {
-    this.angle = 0.0;
-    this.bias = 0;
-
-    this.P00 = 0;
-    this.P01 = 0;
-    this.P10 = 0;
-    this.P11 = 0;
-  }
-
-  filter(accAngle, gyroRate, dt) {
-    this.angle += dt * (gyroRate - this.bias);
-
-    this.P00 += -dt * (this.P10 + this.P01) + this.Q_angle * dt;
-    this.P01 += -dt * this.P11;
-    this.P10 += -dt * this.P11;
-    this.P11 += + this.Q_gyro * dt;
-
-    let axis = accAngle - this.angle;
-    let S = this.P00 + this.R_angle;
-    let K0 = this.P00 / S;
-    let K1 = this.P10 / S;
-
-    this.angle += K0 * axis;
-    this.bias  += K1 * axis;
-
-    this.P00 -= K0 * this.P00;
-    this.P01 -= K0 * this.P01;
-    this.P10 -= K1 * this.P00;
-    this.P11 -= K1 * this.P01;
-
-    return this.angle;
-  }
-};
-
 class OriSensor {
         constructor() {
         const sensor = new RelativeOrientationSensor({ frequency: sensorfreq });
@@ -466,8 +422,8 @@ function buildCameraPath(dataArray) {    //Build the shaky camera path from the 
                 {
                         oriDiff = {"roll": ori.roll-oriInitial.roll, "pitch": ori.pitch-oriInitial.pitch, "yaw": ori.yaw-oriInitial.yaw};
                         //console.log(oriDiff.yaw);
-                        cameraCoord.x = (2/2)* Math.sin(oriDiff.yaw*2) * canvas.width;
-                        cameraCoord.y = (2/2)* Math.sin(oriDiff.roll*2) * canvas.height;
+                        cameraCoord.x = (2/2)* Math.sin(oriDiff.yaw) * canvas.width;
+                        cameraCoord.y = (2/2)* Math.sin(oriDiff.roll) * canvas.height;
                         var b = new Object;     //need to push by value
                         Object.assign(b, cameraCoord);
                         cameraPath.push(b);
